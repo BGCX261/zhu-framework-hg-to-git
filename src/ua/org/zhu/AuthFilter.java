7a0e18478161692c032ae5f9331594f583672b95
zhu <zhu@zhu.org.ua>
2010-12-25 14:01:23 +0200

    В фильтре AuthFilter реализован метод userAuth, который коннектится с MySQL и проверяет наличие пользователя с таким логином и паролем в базе. И еще много-много всякого, что по идее полностью закрывает тикет №1.
    Fixes issue 1

--- src/ua/org/zhu/AuthFilter.java
+++ src/ua/org/zhu/AuthFilter.java
@@ -1,6 +1,7 @@
 package ua.org.zhu;
 
 import java.io.IOException;
+import java.sql.*;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -16,7 +17,8 @@ public class AuthFilter
 	implements Filter
 {
 	private static final String ADMIN_ZONE = "/admin";
-	private static final String LOGIN_URI = "/admin/login";
+	private static final String LOGIN_URI = "/admin/login.jsp";
+	private static final String ADMIN_URI = "/admin/index.jsp";
 	private static final String SESSION_LOGIN = "login";
 	
 	private static final String ACTION_LOGOUT = "logout";
@@ -35,54 +37,87 @@ public class AuthFilter
 		m_response = response;
 		
 		HttpServletRequest httpRequest = (HttpServletRequest)request;
-		System.out.println("new request: " + httpRequest.getRequestURI());
-		if (httpRequest.getRequestURI().startsWith(ADMIN_ZONE))
+		System.out.println("Request: " + httpRequest.getRequestURI());
+		HttpSession httpSession = httpRequest.getSession(true);
+		if (httpSession.getAttribute(SESSION_LOGIN) != null)
 		{
-			HttpSession httpSession = httpRequest.getSession(true);
-			if (httpSession.getAttribute(SESSION_LOGIN) != null)
+			System.out.println("User: " + httpSession.getAttribute(SESSION_LOGIN));
+			if (httpRequest.getParameter(ACTION_LOGOUT) != null && httpRequest.getParameter(ACTION_LOGOUT).equals("true"))
 			{
-				if (httpRequest.getParameter(ACTION_LOGOUT) != null && httpRequest.getParameter(ACTION_LOGOUT).equals("true"))
-				{
-					httpSession.invalidate();
+				httpSession.invalidate();
+				redirectTo(LOGIN_URI);
+			} else 
+				chain.doFilter(request, response);
+		} else {
+			System.out.println("Anonymous user");
+						
+			if (httpRequest.getRequestURI().startsWith(ADMIN_ZONE)) {
+				if (httpRequest.getParameter(FORM_LOGIN) != null && httpRequest.getParameter(FORM_PASS) != null && !httpRequest.getParameter(FORM_LOGIN).equals("") && !httpRequest.getParameter(FORM_PASS).equals("")) {
+					if (userAuth(httpRequest.getParameter(FORM_LOGIN), httpRequest.getParameter(FORM_PASS))) {
+						httpSession.setAttribute(SESSION_LOGIN, httpRequest.getParameter(FORM_LOGIN));
+						System.out.println("User " + httpRequest.getParameter(FORM_LOGIN) + " logged in");
+						redirectTo(ADMIN_URI);
+					} else {
+						redirectTo(LOGIN_URI+"?auth=false");
+					}
+				} else {
+					System.out.println("Not logged in. Could not enter to admin page.");
 					redirectTo(LOGIN_URI);
 				}
-				else
-					chain.doFilter(request, response);
-			}
-			else 
-			{
-				if (httpRequest.getParameter(FORM_LOGIN) != null && httpRequest.getParameter(FORM_PASS) != null && !httpRequest.getParameter(FORM_LOGIN).equals("") && !httpRequest.getParameter(FORM_PASS).equals(""))
-				{
-					// Тут должна быть авторизация
-					httpSession.setAttribute(SESSION_LOGIN, httpRequest.getParameter(FORM_LOGIN));
-				}
-				else 
-					redirectTo(LOGIN_URI);
-			}
-			
+			} else 
+				chain.doFilter(request, response);
 		}
-		chain.doFilter(request, response);
 	}
 	
-	private void redirectTo(String URI)
-		throws IOException, ServletException
-	{
-		RequestDispatcher dispatcher = m_Config.getServletContext().getContext("/").getRequestDispatcher(LOGIN_URI);
+	private void redirectTo(String URI) throws IOException, ServletException {
+		RequestDispatcher dispatcher = m_Config.getServletContext().getContext("/").getRequestDispatcher(URI);
 		dispatcher.forward(m_request, m_response);
 	}
+	
+	private boolean userAuth(String login, String password) {
+		ResultSet rs = null;
+		Statement st = null;
+		Connection con = null;
+		
+		try {
+			con = DriverManager.getConnection("jdbc:mysql:///zhu-framework", "zhu", "zhu");
+			if (con.isClosed()) {
+				System.out.println("Connection to MySQL server failed ");
+				return false;
+			}
+			
+			System.out.println("Successfully connected to MySQL server");
+			st = con.createStatement();
+			rs = st.executeQuery("SELECT * FROM user WHERE login = '" + login + "' AND password = '" + password + "'");
+			
+			if (rs.next())
+				return true;
+			else
+				return false;
+		} catch (Exception e) {
+			System.err.println("Exception: " + e.getMessage());
+			return false;
+		} finally {
+			try {
+				if(rs != null)
+					rs.close();
+				if(st != null)
+					st.close();
+		        if(con != null)
+		        	con.close();
+			} catch (SQLException e) {
+			}
+		}
+	}
 
-    public AuthFilter()
-    {
+    public AuthFilter() {
     }
 
-	public void init(FilterConfig fConfig)
-		throws ServletException
-	{
+	public void init(FilterConfig fConfig) throws ServletException {
 		m_Config = fConfig;
 	}
 
-	public void destroy()
-	{
+	public void destroy(){
 	}
 
 }
